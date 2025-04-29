ce146e8d6ea9b696f2b55210c94da3f391a76699
HirotoBauer <hiroto.bauer.p2@dc.tohoku.ac.jp>
2025-04-29 13:37:15 +0900

    bug fixes

--- conus_snodas_comparison/data_comparison/make_yearly.py
+++ conus_snodas_comparison/data_comparison/make_yearly.py
@@ -8,21 +8,26 @@ def take_yearly_avg(data_paths, water_year, var):
     print(f"started smoothing data for {water_year}")
 
     with xr.open_mfdataset(
-        data_paths, combine="by_coords", chunks={"time": 100, "lat": 500, "lon": 500}
+        data_paths,
+        combine="by_coords",
+        chunks="auto",
     ) as ds:
-        yearly_avg = ds.mean(dim="time")
+        yearly_avg = ds.mean(dim="Time")
 
         # make sure the name of the dataset is corrrect
         file_name = f"snodas_{var}_{water_year}_yearly_avg.nc"
-        save_dir = Path("/kaiganJ/hiroto/python_scripts/conus_yearly/")
+        save_dir = Path("/kaiganJ/hiroto/conus_yearly/")
         yearly_avg.to_netcdf(save_dir / file_name)
 
 
-data_list = pd.read_csv(Path("/kaiganJ/hiroto/file_list/conus_file_list.csv/"))
+data_list = pd.read_csv(
+    Path("/kaiganJ/hiroto/file_list/conus_file_list_corrupted_check.csv/")
+)
 
 var = "SNOWH"
 
 var_list = data_list.loc[data_list["variable"] == var]
+var_list = var_list.loc[~var_list["corrupted"]]
 
 for year in var_list["water_year"].unique():
     year_files = var_list.loc[var_list["water_year"] == year]
--- conus_snodas_comparison/data_comparison/quantile_splitter_new.py
+++ conus_snodas_comparison/data_comparison/quantile_splitter_new.py
@@ -9,37 +9,50 @@ import datetime
 
 
 def drop_coords(ds):
-    return ds.drop_vars(["XLAT", "XLONG"])
+    return ds.drop_vars(["lat", "lon"])
 
 
 # takes the yearly quantile then combined them later so that I dont run out of memory
 def annual_quantiles(data_paths, var, quantile):
+    print("started calculation of quantile cutoff")
+
+    output_dir = Path("C:/Users/noodl/Desktop/usa_snow/processed_data")
+    output_path = output_dir / f"snodas_{var}_{int(quantile * 100)}th_full.nc"
+
     years = data_paths["water_year"].unique()
     temp_files = []
 
     example_coords = xr.open_dataset(data_paths["file_path"].iloc[0])
-    lat = example_coords["XLAT"]
-    lon = example_coords["XLONG"]
+    lat = example_coords["lat"]
+    lon = example_coords["lon"]
     example_coords.close()
 
     for yr in years:
+        print(f"Computing {yr} quantile... ", end="")
         # select file paths for that year
         yearly = data_paths.loc[data_paths["water_year"] == yr, "file_path"]
         file_list = yearly.tolist()
 
+        # ds = xr.open_mfdataset(
+        #     file_list,
+        #     combine="by_coords",
+        #     # preprocess=drop_coords,
+        #     chunks={"time": 500, "y": 200, "x": 200},
+        # )
         ds = xr.open_mfdataset(
             file_list,
-            combine="by_coords",
-            preprocess=drop_coords,
-            chunks={"Time": 500, "lat": 200, "lon": 200},
+            combine="nested",
+            concat_dim="time",
+            coords="minimal",
+            data_vars="minimal",
+            chunks={"time": 500, "y": 200, "x": 200},
+            parallel=True,
         )
 
-        ds6 = ds.sel(Time=ds.Time.dt.hour == 6).fillna(0).astype("float32")
+        ds = ds.assign_coords(lat=lat, lon=lon)
 
-        ds6 = ds6.assign_coords(lat=lat, lon=lon)
-
-        ds6 = ds6.chunk({"Time": -1})
-        qyr = ds6[var].quantile(quantile, dim="Time")
+        ds = ds.chunk({"time": -1})
+        qyr = ds[var].quantile(quantile, dim="time")
 
         fn = f"quant_{var}_{yr}.nc"
 
@@ -47,16 +60,21 @@ def annual_quantiles(data_paths, var, quantile):
         qyr.to_netcdf(fn)  # small 2D file
         temp_files.append(fn)
 
+        print("Done")
+
         ds.close()
-        del ds, ds6, qyr
+        del ds, qyr
 
-    # Now load the ~20 files and stack on “year”
+    # Now load all files and stack on “year”
+    print("Combining yearly quantiles and computing final quantile... ", end="")
     yrs = xr.open_mfdataset(temp_files, combine="nested", concat_dim="year")
-
     yrs = yrs.chunk({"year": -1})
+
     final = yrs[var].quantile(quantile, dim="year")
 
-    final.to_netcdf(f"snodas_{var}_{int(quantile * 100)}th_full.nc")
+    final.to_netcdf(output_path)
+    print("Done")
+    print(f"Final quantile file saved to: {output_path}")
 
 
 file_list_dir = Path(
